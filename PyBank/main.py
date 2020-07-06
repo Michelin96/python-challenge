3afa50dd88f447be391681b805cc4262c59dde99
MichelleH <voirdire96@gmail.com>
2020-07-06 13:08:48 -0700

    sudo code and plan for PyBank

---
+++ PyBank/main.py
@@ -0,0 +1,32 @@
+import csv
+#import any additional libraries for odd functions
+
+#open the budget_data file for reading
+#use the activity examples for getting the headers from the file into a list 
+#put all the budget data in a list that can be indexed
+#creating a list of each colum of data to be analyzed
+
+#use the month column to get the total number of months 
+
+#  use the  "Profit/Losses"  column to caluculate the total over the entire period
+
+#  use the "Profit/Losses" column to find the average of the changes over the entire period
+
+#   * The greatest increase in profits (date and amount) over the entire period
+
+#   * The greatest decrease in losses (date and amount) over the entire period
+
+# * As an example, your analysis should look similar to the one below:
+
+#   ```text
+#   Financial Analysis
+#   ----------------------------
+#   Total Months: 86
+#   Total: $38382578
+#   Average  Change: $-2315.12
+#   Greatest Increase in Profits: Feb-2012 ($1926159)
+#   Greatest Decrease in Profits: Sep-2013 ($-2196167)
+#   ```
+
+# print the analysis to the terminal and export a fin_results.txt file with the results.
+# fin_results.txt needs to be opened for writing and then closed after writing
